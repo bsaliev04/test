1bc0ac5bd8b4765578c438f3cf1e1ec9f540373b
Maner Niyet <manerniyet15@gmail.com>
2021-12-20 23:08:15 -0500

    filterTodo func added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const todoInput = document.querySelector(".todo-input")
 const todoButton = document.querySelector(".todo-button")
 const todoList = document.querySelector(".todo-list") //ul
-
+const filterOption = document.querySelector(".filter-todo")
 
 //functions
 const addTodo = (event) => {
@@ -45,7 +45,32 @@ const deleteComplete = (event) => {
         todo.classList.toggle("completed")
     }
 }
+
+const filterTodo = (event) => {
+    const todos = todoList.childNodes
+    todos.forEach((todo) => {
+        switch (event.target.value) {
+            case "all": 
+                todo.style.display = "flex"
+                break;
+            case "completed": 
+                if (todo.classList.contains("completed")) {
+                    todo.style.display = "flex"
+                } else {
+                    todo.style.display = "none"
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains("completed")) {
+                    todo.style.display = "flex"
+                } else {
+                    todo.style.display = "none"
+                }
+                break; 
+        }
+    })
+}
 //event-listeners
 todoButton.addEventListener("click", addTodo)
 todoList.addEventListener("click", deleteComplete)
-
+filterOption.addEventListener("click", filterTodo)
